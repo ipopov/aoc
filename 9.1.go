8db0c356a0dbbf15ec7fdff7daf1605546eb528c
ipopov <you@example.com>
2021-12-11 16:00:46 -0500

    unnamed commit

---
+++ 9.1.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+import "os"
+import "bufio"
+
+func lowest(b [][]byte, i, j int) bool {
+	li, lj := len(b), len(b[0])
+	for _, x := range []struct{ x, y int }{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} {
+		ii, jj := i+x.x, j+x.y
+		if ii < 0 || ii >= li || jj < 0 || jj >= lj {
+			continue
+		}
+		if b[i][j] >= b[ii][jj] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	board := [][]byte{}
+	for scanner.Scan() {
+    var xxx []byte
+    xxx = append(xxx, scanner.Bytes()...)
+		board = append(board, xxx)
+	}
+	li, lj := len(board), len(board[0])
+
+	risk := 0
+
+	for i := 0; i < li; i++ {
+		for j := 0; j < lj; j++ {
+			if lowest(board, i, j) {
+				r := 1 + int(board[i][j]-byte('0'))
+        fmt.Printf("%c", board[i][j]-byte('0')+byte('a'))
+				risk += r
+			} else {
+        fmt.Printf("%c", board[i][j])
+      }
+		}
+    fmt.Print("\n")
+	}
+
+	fmt.Println(risk)
+}
