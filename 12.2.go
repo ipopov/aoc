6d1334c6d377f37cdbff221b81b87eb52771f5e2
ipopov <you@example.com>
2021-12-17 18:09:27 -0500

    unnamed commit

---
+++ 12.2.go
@@ -0,0 +1,70 @@
+package main
+
+import "fmt"
+import "strings"
+import "os"
+import "bufio"
+
+type graph map[string][]string
+
+type visited struct {
+	xs []string
+}
+
+func (v *visited) add(s string) {
+	if s[0] >= 'A' && s[0] <= 'Z' {
+		return
+	}
+	v.xs = append(v.xs, s)
+}
+
+func (v *visited) any_doubled() bool {
+	for i := 0; i < len(v.xs); i++ {
+		for j := i + 1; j < len(v.xs); j++ {
+			if v.xs[i] == v.xs[j] {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (v *visited) contains(s string) bool {
+	for _, x := range v.xs {
+		if x == s {
+			return s == "start" || v.any_doubled()
+		}
+	}
+	return false
+}
+
+func traverse(g *graph, start string, v visited) int {
+	if start == "end" {
+		return 1
+	}
+
+	ret := 0
+	v.add(start)
+	for _, n := range (*g)[start] {
+		if v.contains(n) {
+			continue
+		}
+		ret += traverse(g, n, v)
+	}
+	return ret
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	g := graph{}
+	//var x int
+	for scanner.Scan() {
+		s := strings.Split(scanner.Text(), "-")
+		g[s[0]] = append(g[s[0]], s[1])
+		g[s[1]] = append(g[s[1]], s[0])
+	}
+	fmt.Println(g)
+	fmt.Println(traverse(&g, "start", visited{}))
+
+}
