7e74763795ab1e88fca5a908a67855ceed319de1
ipopov <you@example.com>
2021-12-18 11:50:16 -0500

    unnamed commit

--- 14.1.go
+++ 14.1.go
@@ -6,56 +6,56 @@ import "bufio"
 import "strings"
 
 func f(m map[string]byte, s string) string {
-  var b strings.Builder
-  b.WriteByte(s[0])
-  for i := 1; i < len(s); i++ {
-    if r, ok := m[s[i-1:i+1]]; ok {
-      b.WriteByte(r)
-    }
-    b.WriteByte(s[i])
-  }
-  return b.String()
+	var b strings.Builder
+	b.WriteByte(s[0])
+	for i := 1; i < len(s); i++ {
+		if r, ok := m[s[i-1:i+1]]; ok {
+			b.WriteByte(r)
+		}
+		b.WriteByte(s[i])
+	}
+	return b.String()
 }
 
-func max_element(m map[byte]int, compare func(int, int)bool ) byte {
-  var max_t byte
-  var max_v int 
-  for k, v := range m {
-    max_t = k
-    max_v = v
-    break
-  }
-  for k, v := range m {
-    if compare(max_v, v) {
-      max_t = k
-      max_v = v
-    }
-  }
-  return max_t
+func max_element(m map[byte]int, compare func(int, int) bool) byte {
+	var max_t byte
+	var max_v int
+	for k, v := range m {
+		max_t = k
+		max_v = v
+		break
+	}
+	for k, v := range m {
+		if compare(max_v, v) {
+			max_t = k
+			max_v = v
+		}
+	}
+	return max_t
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-  m := map[string]byte {}
-  scanner.Scan() 
-  seed := scanner.Text()
-  scanner.Scan() 
+	m := map[string]byte{}
+	scanner.Scan()
+	seed := scanner.Text()
+	scanner.Scan()
 	for scanner.Scan() {
-    var from string
-    var to byte
+		var from string
+		var to byte
 		fmt.Sscanf(scanner.Text(), "%s -> %c", &from, &to)
-    m[from] = to
+		m[from] = to
 	}
-  for i := 0; i < 10; i++ {
-    seed = f(m, seed)
-  }
-  cs := map[byte]int{}
-  for _, c := range []byte(seed) {
-    cs[c] += 1
-  }
-  max := max_element(cs, func(x, y int) bool { return x < y })
-  min := max_element(cs, func(x, y int) bool { return x > y })
+	for i := 0; i < 10; i++ {
+		seed = f(m, seed)
+	}
+	cs := map[byte]int{}
+	for _, c := range []byte(seed) {
+		cs[c] += 1
+	}
+	max := max_element(cs, func(x, y int) bool { return x < y })
+	min := max_element(cs, func(x, y int) bool { return x > y })
 
-  fmt.Println(cs)
-  fmt.Println(cs[max] - cs[min])
+	fmt.Println(cs)
+	fmt.Println(cs[max] - cs[min])
 }
--- 14.2.go
+++ 14.2.go
@@ -6,72 +6,70 @@ import "bufio"
 
 type state map[string]int
 
-func to_state(s string)state {
-  ret := state{}
-  ret[string([]byte{'_', s[0]})] += 1
-  for i := 1; i < len(s)-2; i++ {
-    ret[s[i:i+2]] += 1
-  }
-  ret[string([]byte{s[len(s)-1], '_'})] += 1
-  return ret
+func to_state(s string) state {
+	ret := state{}
+	ret[string([]byte{'_', s[0]})] += 1
+	for i := 0; i < len(s)-1; i++ {
+		ret[s[i:i+2]] += 1
+	}
+	ret[string([]byte{s[len(s)-1], '_'})] += 1
+	return ret
 }
 
 func g(m map[string]byte, s state) state {
-  ret := state{}
-  for k, v := range s {
-    if c, ok := m[k]; ok {
-      ret[string([]byte{k[0], c})] += v
-      ret[string([]byte{c,k[1]})] += v
-    } else {
-      ret[k] += v
-    }
-  }
-  return ret
+	ret := state{}
+	for k, v := range s {
+		if c, ok := m[k]; ok {
+			ret[string([]byte{k[0], c})] += v
+			ret[string([]byte{c, k[1]})] += v
+		} else {
+			ret[k] += v
+		}
+	}
+	return ret
 }
 
-func max_element(m map[byte]int, compare func(int, int)bool ) byte {
-  var max_t byte
-  var max_v int 
-  for k, v := range m {
-    max_t = k
-    max_v = v
-    break
-  }
-  for k, v := range m {
-    if compare(max_v, v) {
-      max_t = k
-      max_v = v
-    }
-  }
-  return max_t
+func max_element(m map[byte]int, compare func(int, int) bool) byte {
+	var max_t byte
+	var max_v int
+	for k, v := range m {
+		max_t = k
+		max_v = v
+		break
+	}
+	for k, v := range m {
+		if compare(max_v, v) {
+			max_t = k
+			max_v = v
+		}
+	}
+	return max_t
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-  m := map[string]byte {}
-  scanner.Scan() 
-  seed := scanner.Text()
-  scanner.Scan() 
+	m := map[string]byte{}
+	scanner.Scan()
+	seed := scanner.Text()
+	scanner.Scan()
 	for scanner.Scan() {
-    var from string
-    var to byte
+		var from string
+		var to byte
 		fmt.Sscanf(scanner.Text(), "%s -> %c", &from, &to)
-    m[from] = to
+		m[from] = to
 	}
-  s := to_state(seed)
-  for i := 0; i < 10; i++ {
-    s = g(m, s)
-  }
-  fmt.Println(s)
-  cs := map[byte]int{}
-  for k, v := range s {
-    cs[k[0]] += v
-    cs[k[1]] += v
-  }
-  delete(cs, '_')
-  max := max_element(cs, func(x, y int) bool { return x < y })
-  min := max_element(cs, func(x, y int) bool { return x > y })
+	s := to_state(seed)
+	for i := 0; i < 40; i++ {
+		s = g(m, s)
+	}
+	cs := map[byte]int{}
+	for k, v := range s {
+		cs[k[0]] += v
+		cs[k[1]] += v
+	}
+	delete(cs, '_')
+	max := max_element(cs, func(x, y int) bool { return x < y })
+	min := max_element(cs, func(x, y int) bool { return x > y })
 
-  fmt.Println(cs)
-  fmt.Println((cs[max] - cs[min])/2)
+	fmt.Println((cs[max] - cs[min]) / 2)
 }
