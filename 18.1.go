b4e62a4a782c72ec6781346820af941575d7711d
ipopov <popov.ivo@gmail.com>
2022-11-17 09:13:42 -0500

    unnamed commit

--- 18.1.go
+++ 18.1.go
@@ -4,67 +4,174 @@ import "fmt"
 import "os"
 import "bufio"
 
-type compute_min struct {
-	seen_first bool
-	min        int
+type node struct {
+	isLeaf             bool
+	value               int
+	parent, left, right *node
 }
 
-func (x *compute_min) add(i int) {
-	if !x.seen_first {
-		x.seen_first = true
-		x.min = i
+func make_node(l, r *node) *node {
+	ret := &node{
+		isLeaf: false,
+		left:    l,
+		right:   r,
 	}
-	if i < x.min {
-		x.min = i
+	ret.left.parent = ret
+	ret.right.parent = ret
+	return ret
+}
+
+type iterator struct {
+  n *node
+  depth int
+}
+
+func make_leaf(v int) *node {
+	return &node{
+		isLeaf: true,
+		value:   v,
 	}
 }
 
-type compute_max struct {
-	seen_first bool
-	max        int
+func descend_left(i iterator) iterator {
+	for ; !i.n.isLeaf; {
+    i.n = i.n.left 
+    i.depth++
+	}
+  return i
 }
 
-func (x *compute_max) add(i int) {
-	if !x.seen_first {
-		x.seen_first = true
-		x.max = i
+//func left(n *node) *node {
+//	p := n.parent
+//	if p == nil {
+//		return nil
+//	}
+//	for n == p.left {
+//		p, n = p.parent, p
+//		if p == nil {
+//			return nil
+//		}
+//	}
+//	q := p.left
+//	for ; !q.isLeaf; q = q.right {
+//	}
+//	return q
+//}
+//
+func right(i iterator) iterator {
+	p := i.n.parent
+	if p == nil {
+		return iterator{
+      n: nil,
+    }
 	}
-	if i > x.max {
-		x.max = i
+	for i.n == p.right {
+		p, i.n = p.parent, p
+    i.depth--
+		if p == nil {
+      return iterator{
+        n: nil,
+      }
+		}
 	}
+  i.n = p.right
+  return descend_left(i)
+}
+
+type tokenType int
+
+const (
+	Open  tokenType = 0
+	Close           = 1
+	Int             = 2
+)
+
+type token struct {
+	t tokenType
+	i int
+}
+
+func parse(tokens *[]token) *node {
+	head := (*tokens)[0]
+	switch head.t {
+	case Int:
+		ret := make_leaf(head.i)
+		*tokens = (*tokens)[1:]
+		return ret
+	case Open:
+		*tokens = (*tokens)[1:]
+		l := parse(tokens)
+		r := parse(tokens)
+		*tokens = (*tokens)[1:]
+		return make_node(l, r)
+	}
+	panic("")
+}
+
+func tokenize(in []byte) []token {
+	ret := []token{}
+	for len(in) != 0 {
+		if in[0] == '[' {
+			ret = append(ret, token{t: Open})
+      in = in[1:]
+      continue
+		}
+		if in[0] == ']' {
+			ret = append(ret, token{t: Close})
+      in = in[1:]
+      continue
+		}
+		if in[0] == ',' {
+      in = in[1:]
+      continue
+		}
+    i := 0
+    for in[0] >= '0' && in[0] <= '9' {
+      i = i*10 + int(in[0] - '0')
+      in = in[1:]
+    }
+		ret = append(ret, token{t: Int, i:i})
+	}
+	return ret
+}
+
+func maybeSplit(n *node) bool {
+  if (n.value < 10) {
+    return false
+  }
+  l:= make_leaf(n.value / 2)
+  r:= make_leaf(n.value / 2)
+  l.parent = n
+  r.parent = n
+  n.isLeaf= false
+  n.left = l
+  n.right = r
+  
+  return true
 }
 
 func main() {
+	tokens := tokenize([]byte("[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]"))
+	fmt.Printf("%u\n", tokens)
+	n := parse(&tokens)
+  root_iterator := iterator {
+  n: n,
+  depth: 0,
+  }
+
+	//for x := descend_left(root_iterator); x != nil; x = right(x) {
+	//	if maybeSplit(x) {
+  //    break
+  //  }
+	//}
+	for x := descend_left(root_iterator); x.n != nil; x = right(x) {
+		fmt.Printf("%d: %d\n", x.depth, x.n.value)
+	}
+
+	fmt.Printf("%u\n", n)
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	var x, u, y, v int
 	fmt.Sscanf(scanner.Text(), "target area: x=%d..%d, y=%d..%d", &x, &u, &y, &v)
 	fmt.Println(x, u, y, v)
-
-	type pair struct {
-		x, y int
-	}
-	m := map[pair]bool{}
-	for vy := y; vy <= -y; vy++ {
-		for vx := 0; vx <= u; vx++ {
-			vx_i, vy_i := vx, vy
-			x_i, y_i := 0, 0
-			for t := 0; ; t++ {
-				x_i += vx_i
-				y_i += vy_i
-				if vx_i != 0 {
-					vx_i--
-				}
-				vy_i--
-				if x_i >= x && x_i <= u && y_i >= y && y_i <= v {
-					m[pair{vx, vy}] = true
-				}
-				if y_i < y {
-					break
-				}
-			}
-		}
-	}
-	fmt.Println(m)
-	fmt.Println(len(m))
 }
